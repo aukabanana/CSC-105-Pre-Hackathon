a14f1d6e30f76cefe9854947268f1d2acee7f514
xiangxiang818 <navipin.hongp@mail.kmutt.ac.th>
2026-05-19 00:44:55 +0700

    feat: finish UI dashboard page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginPage from './pages/loginPage.tsx'
+// import App from '../App.tsx'
 import './index.css'
-import App from './App.tsx'
+import DashboardPage from './pages/dashboardPage.tsx'
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <LoginPage />   
+    },
+    {
+        path: '/dashboard',
+        element: <DashboardPage />   
+    },
+])
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 )
