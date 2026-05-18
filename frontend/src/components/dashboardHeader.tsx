c3e745ae2e217dcce6719ec17464b807826c8a37
xiangxiang818 <navipin.hongp@mail.kmutt.ac.th>
2026-05-18 15:23:12 +0700

    feat: set structure of header

---
+++ frontend/src/components/dashboardHeader.tsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+    faHouse
+} from '@fortawesome/free-solid-svg-icons'
+
+function DashboardHeader() {
+    return(
+        <div>
+            <FontAwesomeIcon icon={faHouse}/>
+        </div>
+    )
+}
+export default DashboardHeader
--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,7 +1,9 @@
+import DashboardHeader from "../components/dashboardHeader"
 function LoginPage() {
     return(
         <div>
             <h1>Hello</h1>
+            <DashboardHeader />
         </div>
     )
 }
