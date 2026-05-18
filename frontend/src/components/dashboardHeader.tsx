719a590b5da0afca4fbd2054279da6c7a22288c4
xiangxiang818 <navipin.hongp@mail.kmutt.ac.th>
2026-05-18 16:58:19 +0700

    feat: create logedout session

--- frontend/src/components/dashboardHeader.tsx
+++ frontend/src/components/dashboardHeader.tsx
@@ -1,20 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import  type { Header }  from "../types/header";
 import {
     faHouse
 } from '@fortawesome/free-solid-svg-icons'
 
-export type HeaderProps = {
-    title: string;
-}
 
-function DashboardHeader({title}: HeaderProps) {
+function DashboardHeader({title, username}: Header) {
     return(
-        <div className="flex p-2 rounded-full shadow-(--shadow-lg) bg-(--bg-card)">
+        <div className="flex p-2 rounded-full shadow-(--shadow-lg) bg-(--bg-card) justify-between">
             <div className="flex flex-row items-center gap-5">
                 <div className="bg-gray-300 rounded-full p-2 flex items-center justify-center w-12 h-12">
                     <FontAwesomeIcon icon={faHouse} className="text-2xl" />
                 </div>
                 <h2 className="text-2xl font-bold">{title}</h2>
+            </div>
+            <div className="flex flex-row items-center gap-5 pr-5">
+                <h2 className="text-2xl font-bold">{username}</h2>
+                {username && <FontAwesomeIcon icon={faHouse} className="text-2xl" />}
             </div>
         </div>
     )
