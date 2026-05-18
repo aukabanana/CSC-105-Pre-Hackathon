71ef2035376a90ec4f608197fbdfecc8e2a532b9
xiangxiang818 <navipin.hongp@mail.kmutt.ac.th>
2026-05-18 16:45:09 +0700

    feat: create login page

--- frontend/src/components/dashboardHeader.tsx
+++ frontend/src/components/dashboardHeader.tsx
@@ -3,10 +3,19 @@ import {
     faHouse
 } from '@fortawesome/free-solid-svg-icons'
 
-function DashboardHeader() {
+export type HeaderProps = {
+    title: string;
+}
+
+function DashboardHeader({title}: HeaderProps) {
     return(
-        <div>
-            <FontAwesomeIcon icon={faHouse}/>
+        <div className="flex p-2 rounded-full shadow-(--shadow-lg) bg-(--bg-card)">
+            <div className="flex flex-row items-center gap-5">
+                <div className="bg-gray-300 rounded-full p-2 flex items-center justify-center w-12 h-12">
+                    <FontAwesomeIcon icon={faHouse} className="text-2xl" />
+                </div>
+                <h2 className="text-2xl font-bold">{title}</h2>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,9 +1,48 @@
 import DashboardHeader from "../components/dashboardHeader"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+    faUser,
+    faLock
+} from '@fortawesome/free-solid-svg-icons'
+
 function LoginPage() {
-    return(
-        <div>
-            <h1>Hello</h1>
-            <DashboardHeader />
+    return (
+        <div className="flex flex-col p-10 bg-(--bg-main)">
+            <DashboardHeader title="Login" />
+
+            <div className="flex justify-center items-center min-h-screen">
+                <div className="flex flex-col items-center gap-10 p-10 bg-(--bg-card) rounded-3xl shadow-(--shadow-lg)">
+                    <div className="flex flex-col items-center gap-5">
+                        <h3 className="text-2xl font-bold">Welcome Back</h3>
+                        <p className="text-sm font-medium">Login in to access smart home dashboard</p>
+                    </div>
+                    <div className="flex flex-col gap-5 w-full">
+                        <div className="flex flex-col gap-1.5 ">
+                            <label className="font-bold">Username</label>
+                            <div className="flex flex-row items-center border py-3 px-4 rounded-xl gap-4">
+                                <FontAwesomeIcon icon={faUser} className="text-gray-500"/>
+                                <input type="text" placeholder="Enter username" />
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-1.5 ">
+                            <label className="font-bold">Password</label>
+                            <div className="flex flex-row items-center border py-3 px-4 rounded-xl gap-4">
+                                <FontAwesomeIcon icon={faLock} className="text-gray-500"/>
+                                <input type="text" placeholder="Enter password" />
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="flex flex-col gap-5">
+                        <button className="
+                    text-center bg-(--color-primary) text-(--bg-card) p-3 rounded-xl shadow-(--shadow-button) w-full">
+                            Login
+                        </button>
+                        <p className="font-light text-xs md:text-sm p-2">Manage devices, monitor energy usage and view alerts</p>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
