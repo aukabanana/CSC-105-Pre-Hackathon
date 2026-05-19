08e6f76254d7cb6c6c15029804391733d9b9439a
xiangxiang818 <navipin.hongp@mail.kmutt.ac.th>
2026-05-19 15:42:26 +0700

    style: reponsive modal add device

--- frontend/src/pages/devicesPage.tsx
+++ frontend/src/pages/devicesPage.tsx
@@ -25,38 +25,38 @@ export function DevicePage() {
             </div>
 
             {addDevice && (
-                <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+                <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-20">
 
-                    <div className="flex flex-col bg-(--bg-card) p-10 rounded-2xl md:rounded-3xl shadow-xl min-w-75 w-180 gap-10">
+                    <div className="flex flex-col bg-(--bg-card) p-[clamp(20px,2.5vw,40px)] rounded-2xl md:rounded-3xl shadow-xl min-w-75 w-120 sm:w-140 md:w-180 gap-[clamp(20px,2.5vw,40px)]">
                         <div className="flex flex-col">
-                            <p className="text-lg font-bold">Add New Device</p>
-                            <p>Create a new device for your smart home system</p>
+                            <p className="text-lg font-bold text-[18px] md:text-[22px]">Add New Device</p>
+                            <p className="text-[14px] md:text-[18px] font-light">Create a new device for your smart home system</p>
                         </div>
 
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-[clamp(20px,2.5vw,40px)]">
                             <div className="flex flex-col gap-5">
                                 <div className="flex flex-col gap-1">
-                                    <label className="font-semibold">Name</label>
+                                    <label className="font-semibold text-[14px] md:text-[18px]">Name</label>
                                     <input type="text" placeholder="Enter device name" className="border rounded-xl px-4 py-2 focus:outline-none" />
                                 </div>
                                 <div className="flex flex-col gap-1">
-                                    <label className="font-semibold">Type</label>
+                                    <label className="font-semibold text-[14px] md:text-[18px]">Type</label>
                                     <input type="text" placeholder="Enter device type" className="border rounded-xl px-4 py-2 focus:outline-none" />
                                 </div>
                             </div>
                             <div className="flex flex-col gap-5">
                                 <div className="flex flex-col gap-1">
-                                    <label className="font-semibold">Power Usage</label>
+                                    <label className="font-semibold text-[14px] md:text-[18px]">Power Usage</label>
                                     <input type="text" placeholder="Enter power usage (watt)" className="border rounded-xl px-4 py-2 focus:outline-none" />
                                 </div>
                                 <div className="flex flex-col gap-1">
-                                    <label className="font-semibold">Location</label>
+                                    <label className="font-semibold text-[14px] md:text-[18px]">Location</label>
                                     <input type="text" placeholder="Enter device location" className="border rounded-xl px-4 py-2 focus:outline-none" />
                                 </div>
                             </div>
                         </div>
 
-                        <div className="flex flex-row gap-5 justify-end">
+                        <div className="flex flex-row gap-5 justify-center sm:justify-end md:justify-end ">
                             <button className="cancelButton border-[0.5px] rounded-2xl shadow-(--shadow-cancel) hover:shadow-(--shadow-button) px-5 py-2 cursor-pointer " onClick={() => setAddDevice(false)}>Cancel</button>
                             <button className="rounded-2xl shadow-(--shadow-button) px-5 py-2 bg-(--color-primary) text-(--bg-card) cursor-pointer hover:bg-(--color-primary-hover)">Add Device</button>
                         </div>
