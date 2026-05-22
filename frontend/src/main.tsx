b81a9b07539ee79517576c02ab500ca630a3cd1d
aukabanana <hope25501@gmail.com>
2026-05-22 16:18:57 +0700

    fix: change to using Outlet and set notification path

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,23 +3,25 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import NotificationsPage from './modules/notifications/pages/notificationsPage.tsx'
+import { notificationsPage } from './modules/notifications/routers/notifications.router'
 import ElectricChargePage from './pages/electricChargePage.tsx'
 import DashboardPage from './pages/dashboardPage.tsx'
 import { DevicePage } from './pages/devicesPage.tsx'
+import LoginPage from './pages/loginPage.tsx'
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />
+        element: <App />,
+        children: [...notificationsPage],
+    },
+    {
+        index: true,
+        element: <LoginPage />, 
     },
     {
         path: '/dashboard',
-        element: <DashboardPage /> 
-    },
-    {
-        path: '/notifications',
-        element: <NotificationsPage />   
+        element: <DashboardPage />,
     },
     {
         path: '/devices-controller',
