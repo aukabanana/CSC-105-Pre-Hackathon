60003d9165789acbade572b4befeb9b6e6fb1f08
aukabanana <hope25501@gmail.com>
2026-05-23 17:27:51 +0700

    fix: change integration api end point notifications

--- frontend/src/modules/notifications/apis/notificationApi.tsx
+++ frontend/src/modules/notifications/apis/notificationApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { GetNotificationsResponse, Notification } from "../types/notification";
 
 export async function getNotifications(): Promise<Notification[]> {
-    const response = await axios.get<GetNotificationsResponse>("http://localhost:3000/notifications");
+    const response = await axios.get<GetNotificationsResponse>("http://localhost:3000/api/notifications");
     return response.data.data;
 }
 
@@ -10,7 +10,7 @@ export async function markNotificationAsRead(id: string): Promise<Notification> 
     const response = await axios.patch<{
         message: string;
         data: Notification;
-    }>(`http://localhost:3000/notifications/${id}/read`);
+    }>(`http://localhost:3000/api/notifications/${id}/read`);
 
     return response.data.data;
 }
