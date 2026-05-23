6987296bad97ff35de0c5cd1a17c6c4e35f7d574
aukabanana <hope25501@gmail.com>
2026-05-23 17:37:40 +0700

    fix: remove duplicate backend index.ts code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,10 +15,6 @@ const PORT = process.env.PORT
 
 if (!PORT) throw new Error('PORT is missing in your dotenv file')
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
@@ -29,12 +25,13 @@ app.use(morgan('dev'))
 
 app.use('/api', router);
 app.use(authRouter);
-app.use(errorHandler);
 
 app.use(errorHandler)
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Connected, Welcome to BackEnd jaaaa 👾👾')
 })
